4854f7a505e6039852732824ccc35ea90605535b
Agetorp <elias.agetorp@gmail.com>
2015-02-25 14:34:05 +0100

    Created a file for our helperfunctions with package 'helperfunctions' and added helperfunctions

---
+++ helperfunctions/helperfunctions.go
@@ -0,0 +1,33 @@
+package helperfunctions
+
+import (
+	"strings"
+	"unicode"
+)
+
+func TrimSpacesFromArray(title []string) []string {
+	for i := 0; i < len(title); i++ {
+		title[i] = strings.TrimSpace(title[i])
+	}
+	return title
+}
+
+func TrimSuffixFromWord(word string, suffix string) string {
+	word = strings.TrimSuffix(word, suffix)
+	return word
+}
+
+func StartsWithUppercase(s string) bool {
+
+	return unicode.IsUpper([]rune(s)[0])
+
+}
+
+func StringInSliceIgnoreCase(s string, list []string) bool {
+	for _, str := range list {
+		if strings.ToLower(s) == strings.ToLower(str) {
+			return true
+		}
+	}
+	return false
+}
