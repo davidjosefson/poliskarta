ea4204ae116f7d505d704adb68e7ee9db0de3297
David Josefson <david.josefson@gmail.com>
2015-02-27 18:21:51 +0100

    Removed unnecessary fill-methods and added some comments

--- descriptionsrules/filterdescriptionrules.go
+++ descriptionsrules/filterdescriptionrules.go
@@ -5,37 +5,32 @@ import (
 	"strings"
 )
 
-//Used to filter out Europe road names, like "E6", "E22"
-var europeRoads []string
-
-// var arrayOfTitles []string
-
-//Used to include common words in names of places
-//separated by space, like "Jörgens trappa", "Anna Lindhs plats"
-var validWordsForPlaces []string
-
-//Used to filter out words for roads followed by numbers,
-//like "Lv 598", "väg 112" and the like
-var invalidWordsForRoads []string
+var europeRoads = []string{"E4", "E6", "E10", "E12", "E14", "E16", "E18", "E22", "E45", "E65", "E", "Lv"}
+var validWordsForPlaces = []string{
+	"väg", "gränd", "plats", "gata", "led", "torg", "park", "trappa",
+	"trappor", "bro", "gångbro", "allé", "alle", "aveny", "plan", "kaj",
+	"hamn", "strand", "stig", "backe", "kajen", "hamnen", "holme", "holmar",
+	"dockan", "parkväg", "byväg", "byaväg", "gård", "stråket", "tvärgata",
+	"gårdar", "parkgata", "idrottsväg", "broväg", "vägen", "stationsgata",
+	"hamngata", "bangårdsgata", "fätåg", "kyrkogata", "hage", "stråket", "ö",
+	"träsk", "flygplats", "industriväg", "trappgata", "kärr", "ringvägen"}
+var invalidWordsForRoads = []string{"väg", "Lv", "Länsväg", "länsväg"}
 
 //Rule 1:
 func Rule1(description string) []string {
-	fillEuropeRoads()
-	fillValidWordsForPlaces()
-	fillInvalidWordsForRoads()
-
 	prevWordAdded := false
 
-	//Split on spaces - descWords = array
+	//Split string on spaces - descWords = array
 	descWords := strings.Split(description, " ")
+
+	//Remove spaces from words
 	helperfunctions.TrimSpacesFromArray(&descWords)
 
-	//The resulting array of words after filtering
+	//The resulting array of location words after filtering
 	locationWords := []string{}
 
 	//Loop through the array of words
 	for i := 1; i < len(descWords); i++ {
-
 		currentWord := descWords[i]
 		prevWord := descWords[i-1]
 
@@ -84,28 +79,18 @@ func Rule1(description string) []string {
 		//Check if current word starts with uppercase and is NOT europe road
 		if helperfunctions.StartsWithUppercase(currentWord) {
 			helperfunctions.TrimSuffixesFromWord(&currentWord, ".", ",")
+
 			if !helperfunctions.StringInSliceIgnoreCase(currentWord, europeRoads) {
-				addWordTolocationWords()
 				locationWords = append(locationWords, currentWord)
 				prevWordAdded = true
 			}
+
 		} else {
 			prevWordAdded = false
 		}
 	}
 
 	return locationWords
-}
-func fillEuropeRoads() {
-	europeRoads = []string{"E4", "E6", "E10", "E12", "E14", "E16", "E18", "E22", "E45", "E65", "E", "Lv"}
-}
-
-func fillValidWordsForPlaces() {
-	validWordsForPlaces = []string{"väg", "gränd", "plats", "gata", "led", "torg", "park", "trappa", "trappor", "bro", "gångbro", "allé", "alle", "aveny", "plan", "kaj", "hamn", "strand", "stig", "backe", "kajen", "hamnen", "holme", "holmar", "dockan", "parkväg", "byväg", "byaväg", "gård", "stråket", "tvärgata", "gårdar", "parkgata", "idrottsväg", "broväg", "vägen", "stationsgata", "hamngata", "bangårdsgata", "fätåg", "kyrkogata", "hage", "stråket", "ö", "träsk", "flygplats", "industriväg", "trappgata", "kärr", "ringvägen"}
-}
-
-func fillInvalidWordsForRoads() {
-	invalidWordsForRoads = []string{"väg", "Lv", "Länsväg", "länsväg"}
 }
 
 func currentIndexNotLast(index int, strings []string) bool {
