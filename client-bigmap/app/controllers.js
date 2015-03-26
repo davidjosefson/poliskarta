dfb1ac7b9b16e71a569faa3aa633c0252756cc28
David Josefson <david.josefson@gmail.com>
2015-03-26 12:31:18 +0100

    Removed comments, renamed functions and fixed '/' bug where it would only show a blank site

--- client-bigmap/app/controllers.js
+++ client-bigmap/app/controllers.js
@@ -1,10 +1,11 @@
 "use strict";
-//
-//var bigmapControllers = angular.module('bigmapControllers', []);
-//
-//bigmapControllers.controller('mainCtrl', function(){
-//    this.test = "hej";
-//});
+
+
+/*
+TODO:
+1. Repetera alla areas och skapa länkar för var och en
+2. Fixa routing så att ett klick på en länk tar en till en ny karta och laddar in pins för just det länet
+*/
 
 var bigmapControllers = angular.module("bigmapControllers", []);
 
@@ -16,57 +17,54 @@ bigmapControllers.controller("mainController", ["uiGmapGoogleMapApi", "$scope", 
     vm.areas = {};
     vm.selectedArea = {};
 
+    vm.map = {
+        center: {
+            latitude: 62.1983366,
+            longitude: 17.567198
+        },
+        zoom: 5
+    };
+
+    //Get the list of areas
     $http.get("http://localhost:3000/api/v1/areas/").success(function(data) {
         vm.areas = data;
 
         for (var i = 0; i < vm.areas.areas.length; i++) {
+            //If the route parameter matches one of the fetched areas:
             if (vm.areas.areas[i].value === $routeParams.area) {
                 vm.selectedArea = vm.areas.areas[i];
                 vm.map.center.latitude = vm.selectedArea.latitude;
                 vm.map.center.longitude = vm.selectedArea.longitude;
-                vm.getEvents();
+                vm.map.zoom = 8;
+                
+                //Get events for the chosen area
+                getEvents();
             }
         }
     });
 
-    function onClickTest(title, id) {
-        return function() {
-            for (var i = 0; i < vm.markerlist.length; i++)  {
-                if (id == vm.markerlist[i].id) {
-                    vm.markerlist[i].show = !vm.markerlist[i].show;
-                } else if (vm.markerlist[i].show) {
-                    vm.markerlist[i].show = !vm.markerlist[i].show;
-                }
+    //Get all events for the chosen area and for each event: get the coordinates in a separate get-request
+    function getEvents() {
+        $http.get("http://localhost:3000/api/v1/areas/" + vm.selectedArea.value + "/?limit=10").success(function(data) {
+            var events = data;
+            for (var i = 0; i < events.events.length; i++) {
+                var marker = {
+                    id: i + 100,
+                    eventURI: events.events[i].links[0].href,
+                    title: events.events[i].title,
+                    description: events.events[i].descriptionShort,
+                    coords: {
+                    },
+                    show: false,
+                    onClick: showOrHideMarker(events.events[i].Title, i + 100)
+                };
+                marker.onClick = showOrHideMarker(marker.title, marker.id);
+                getCoordinatesForEvent(i, marker);
             }
-        };
+        });
     }
 
-    function closeAllWindows() {
-        return function() {
-            console.log("Clicked on the map!");
-            //            vm.markerlist[1].show = !vm.markerlist[1].show;
-            // console.log("[1] = " + vm.markerlist[1].title);  
-            for (var i = 0; i < vm.markerlist.length; i++)  {
-                if (vm.markerlist[i].show) {
-                    vm.markerlist[i].show = !vm.markerlist[i].show;
-                }
-            }
-        };
-    }
- 
-    vm.closeAll = closeAllWindows();
-    vm.test = "hej";
-
-    
-
-
-    vm.singleCallToAllEvents = function() {
-        for (var i = 0; i < vm.markerlist.length; i++) {
-          vm.singleEventCall(i);
-        }
-    };
-
-    vm.singleEventCall = function(index, marker) {
+    function getCoordinatesForEvent(index, marker) {
         $http.get(marker.eventURI).success(function(data) {
             var event = data;
 
@@ -80,108 +78,34 @@ bigmapControllers.controller("mainController", ["uiGmapGoogleMapApi", "$scope", 
                 console.log("Added marker: " + marker.title);
             }
         });
-    };
+    }
 
-    /*
-    1. Repetera alla areas och skapa länkar för var och en
-    2. Fixa routing så att ett klick på en länk tar en till en ny karta och laddar in pins för just det länet
-
-    */
-
-    // $http.get("http://localhost:3000/api/v1/areas/skane/?limit=5").success(function(data) {
-        vm.getEvents = function() {
-            $http.get("http://localhost:3000/api/v1/areas/" + vm.selectedArea.value + "/?limit=50").success(function(data) {
-                var events = data;
-                for (var i = 0; i < events.events.length; i++) {
-                    var marker = {
-                        id: i + 100,
-                        eventURI: events.events[i].links[0].href,
-                        title: events.events[i].title,
-                        description: events.events[i].descriptionShort,
-                        coords: {
-                        },
-                        show: false,
-                        onClick: onClickTest(events.events[i].Title, i + 100)
-                    };
-                    marker.onClick = onClickTest(marker.title, marker.id);
-                    vm.singleEventCall(i, marker);
+    function showOrHideMarker(title, id) {
+        return function() {
+            for (var i = 0; i < vm.markerlist.length; i++)  {
+                if (id == vm.markerlist[i].id) {
+                    vm.markerlist[i].show = !vm.markerlist[i].show;
+                } else if (vm.markerlist[i].show) {
+                    vm.markerlist[i].show = !vm.markerlist[i].show;
                 }
-                // vm.singleCallToAllEvents();
-            });
+            }
         };
+    }
 
+    function closeAllMarkerWindows() {
+        return function() {
+            console.log("Clicked on the map!");
+            for (var i = 0; i < vm.markerlist.length; i++)  {
+                if (vm.markerlist[i].show) {
+                    vm.markerlist[i].show = !vm.markerlist[i].show;
+                }
+            }
+        };
+    }
 
-    //    vm.markerlist = [
-    //        {
-    //            id: 50,
-    //            title: "2015-03-07 19:42, Inbrott, Eslöv",
-    //            description: "Inbrott i bostad, Löberöd.",
-    //            coords: {
-    //                latitude: 55.63755,
-    //                longitude: 13.063351
-    //            },
-    //            show: true,
-    //            onClick: onClickTest("2015-03-07 19:42, Inbrott, Eslöv", 50)
-    //        }, {
-    //            id: 70,
-    //            title: "2015-03-07 19:42, Rån, Örkelljunga",
-    //            description: "Bla i bla i bla.",
-    //            coords: {
-    //                latitude: 55.710692,
-    //                longitude: 14.293691
-    //            },
-    //            show: true,
-    //            onClick: onClickTest("2015-03-07 19:42, Rån, Örkelljunga", 70)
-    //    }, {
-    //            id: 90,
-    //            title: "2015-03-07 19:42, Hej, Hejsingborg",
-    //            description: "MMMM mmmmm oooooo.",
-    //            coords: {
-    //                latitude: 55.563947,
-    //                longitude: 13.552114
-    //            },
-    //            show: true,
-    //            onClick: onClickTest("MMMM mmmmm oooooo.", 90)
-    //    }
-    //    ];
-
-    vm.testtest = onClickTest();
-    //    vm.testtest();
-
-
-
-    vm.map = {
-        /*center: {
-            longitude: 56.283333,
-            latitude: 15.116667
-        },
-        zoom: 8*/
-        center: {
-            latitude: 55.60714,
-            longitude: 13.004377
-        },
-        zoom: 8
-            //        events: {
-            //            click: //vm.closeAll()
-            //                closeAllWindows()
-            //        }
-    };
-
-
-    vm.windowOptions = {
-        visible: false
-    };
-
-    vm.onClick = function() {
-        vm.windowOptions.visible = !vm.windowOptions.visible;
-        vm.test = "mjeee";
-    };
-
-    vm.closeClick = function() {
-        vm.windowOptions.visible = false;
-    };
-
-    vm.title = "Window Title!";
+    //Have to register this as a "vm"-function separately, 
+    //otherwise it won't execute when called from areas.html
+    vm.closeAllMarkerWindows = closeAllMarkerWindows();
 
     uiGmapGoogleMapApi.then(function(maps) {
 
